2159ea71beaaf934ecb9ffaa44dd4a2312e142c7
Jeiel Lima Miranda <github.esvlc@slmail.me>
2024-08-12 11:56:39 -0300

    tray
    https://www.electronjs.org/pt/docs/latest/tutorial/tray

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow, Menu, shell } = require('electron');
+const { app, BrowserWindow, Menu, shell, Tray } = require('electron');
 const path = require('path');
 
+let tray = null; // Variável para armazenar a instância da bandeja
+let win = null; // Variável para armazenar a instância da janela
+
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 400,
     height: 400,
     webPreferences: {
@@ -15,7 +18,7 @@ function createWindow() {
 
   win.loadFile('index.html');
 
-  // Menu template with a custom button
+  // Menu template com um botão personalizado
   const menu = Menu.buildFromTemplate([
     {
       label: 'Square Cloud',
@@ -26,16 +29,56 @@ function createWindow() {
             shell.openExternal('https://squarecloud.app/');
           },
         },
+        {
+          label: 'Sair',
+          click: () => {
+            app.quit();
+          },
+        },
       ],
     },
   ]);
 
-  // Set the menu to the window
+  // Definir o menu da aplicação
   Menu.setApplicationMenu(menu);
+
+  // Evento para minimizar a janela ao fechar
+  win.on('close', (event) => {
+    if (!app.isQuiting) {
+      event.preventDefault();
+      win.hide();
+    }
+  });
 }
 
 app.whenReady().then(() => {
   createWindow();
+
+  // Criar a bandeja
+  tray = new Tray(path.join(__dirname, 'logo.png')); // Substitua pelo caminho do seu ícone
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Abrir',
+      click: () => {
+        win.show();
+      },
+    },
+    {
+      label: 'Sair',
+      click: () => {
+        app.isQuiting = true; // Define a flag para indicar que a aplicação deve sair
+        app.quit();
+      },
+    },
+  ]);
+  
+  tray.setToolTip('Minha Aplicação Electron');
+  tray.setContextMenu(contextMenu);
+
+  // Evento de clique na bandeja
+  tray.on('click', () => {
+    win.isVisible() ? win.hide() : win.show();
+  });
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -49,4 +92,3 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 });
-
