98b7437de81f00d815067fcf72f780945142d49f
Jeiel Lima Miranda <github.esvlc@slmail.me>
2024-08-15 11:55:10 -0300

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -4,20 +4,33 @@ const nextButton = document.getElementById('next');
 const prevButton = document.getElementById('prev');
 const trackName = document.getElementById('track-name');
 const volumeControl = document.getElementById('volume');
+const categorySelect = document.getElementById('category-select');
 
-const tracks = [
-  'http://radio.stereoscenic.com/ama-h',
-  'http://radio.stereoscenic.com:80/am-h.mp3',
-  'http://163.172.169.217:80/asp-s',
-  'http://radio.stereoscenic.com:80/am-l.mp3',
-];
+const categories = {
+  Ambientes: [
+    'http://radio.stereoscenic.com/ama-h',
+    'http://radio.stereoscenic.com:80/am-h.mp3',
+    'http://163.172.169.217:80/asp-s',
+    'http://radio.stereoscenic.com:80/am-l.mp3',
+  ],
+  Jazz: [
+    'http://icecast.radiofrance.fr/fip-midfi.mp3',
+    'https://icecast.radiofrance.fr/fipjazz-midfi.mp3',
+    'https://icecast.radiofrance.fr/fipgroove-midfi.mp3',
+  ],
+  Eletronica: [
+    'http://nl.ah.fm:8000/live',
+    'http://fr2.ah.fm:8000/live',
+  ]
+};
 
+let currentCategory = categories.Ambientes;
 let currentTrackIndex = 0;
 
 audio.volume = volumeControl.value;
 
 function playTrack(index) {
-  audio.src = tracks[index];
+  audio.src = currentCategory[index];
   audio.play();
   trackName.textContent = `Playing ${index + 1}`;
   playButton.classList.remove('fa-play');
@@ -40,12 +53,12 @@ playButton.addEventListener('click', () => {
 });
 
 nextButton.addEventListener('click', () => {
-  currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
+  currentTrackIndex = (currentTrackIndex + 1) % currentCategory.length;
   playTrack(currentTrackIndex);
 });
 
 prevButton.addEventListener('click', () => {
-  currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
+  currentTrackIndex = (currentTrackIndex - 1 + currentCategory.length) % currentCategory.length;
   playTrack(currentTrackIndex);
 });
 
@@ -54,3 +67,9 @@ volumeControl.addEventListener('input', (event) => {
 });
 
 volumeControl.value = audio.volume;
+
+categorySelect.addEventListener('change', (event) => {
+  currentCategory = categories[event.target.value];
+  currentTrackIndex = 0;
+  playTrack(currentTrackIndex);
+});
