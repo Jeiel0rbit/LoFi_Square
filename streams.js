0fc9548644e432c33c43258c0e7e7f6a353f0946
Jeiel Lima Miranda <github.esvlc@slmail.me>
2025-01-16 09:02:40 -0300

    Update streams.js

--- streams.js
+++ streams.js
@@ -9,16 +9,19 @@ const categorySelect = document.getElementById("category-select");
 const categories = {
   Ambientes: [
     "http://radio.stereoscenic.com/ama-h",
-    "http://radio.stereoscenic.com:80/am-h.mp3",
-    "http://163.172.169.217:80/asp-s",
-    "http://radio.stereoscenic.com:80/am-l.mp3",
+    "http://radio.stereoscenic.com/ama-s",
+    "http://radio.stereoscenic.com/ama-h",
+    "http://radio.stereoscenic.com/asp-h3",
   ],
   Jazz: [
     "http://icecast.radiofrance.fr/fip-midfi.mp3",
     "https://icecast.radiofrance.fr/fipjazz-midfi.mp3",
     "https://icecast.radiofrance.fr/fipgroove-midfi.mp3",
   ],
-  Eletronica: ["http://nl.ah.fm:8000/live", "http://fr2.ah.fm:8000/live"],
+  Eletronica: [
+    "http://nl.ah.fm:8000/live", 
+    "http://fr2.ah.fm:8000/live",
+  ],
 };
 
 let currentCategory = categories.Ambientes;
